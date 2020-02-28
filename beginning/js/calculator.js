555e53f61b513f080700c641f7425d3dc6d13448
cteichman123 <60764095+cteichman123@users.noreply.github.com>
2020-02-27 20:07:13 -0800

    Add files via upload

---
+++ beginning/js/calculator.js
@@ -0,0 +1,71 @@
+*eslint-env browser*/
+
+//GLOBAL VARIABLES
+let one;
+let two;
+let three;
+let four;
+let five;
+let six;
+let seven;
+let eight;
+let nine;
+let divide;
+let multiply;
+let subtract; 
+let period;
+let add;
+let equal;
+let subtract;
+let divide;
+let result;
+
+
+//HELPER FUNCTION TO GET DOM ELEMENTS
+let $ = fuction (id) {
+    return window.document.getElementById(id);
+}
+
+//GET DOM ELEMENTS
+divide      = window.document.getElementById('divide');
+multiply    = window.document.getElementById('multiply');
+subtract    = window.document.getElementById('subtract');
+divide      = window.document.getElementById('divide');
+add         = window.document.getElementById('add');
+
+
+//CALCULATE THE FUTURE VALUE
+function init(divide, multiply, add) {
+
+result = rate / 12 / 100;
+months = years * 12;
+futureValue = investment;
+
+for(i = 1; i < months; i +=1) {
+    monthlyInterest = futureValue * monthlyRate;
+    futureValue += monthlyInterest; 
+
+}
+
+return futureValue;
+
+//GET THE VALUES OF THE DOM ELEMENTS
+document.addEventListener(event, listener);
+   function = parseFloat (event, listener);
+   function = parseFloat (rate.value);
+    function int = parseInt (years.value);
+    function int. = calculateInvestement (investment.value, rate.value, years.value);
+    function = 'Future value: $' + Math.round(futureValue) + '.00';
+
+});
+
+calculate.event
+
+
+
+
+
+
+
+
+
